69bf5b9f33af695a7a6c3faec12354939d0a4ec4
Manfred Christoph Asare <asare1obeng@gmail.com>
2025-11-09 04:09:37 +0000

    Practice receiving user input in Python and perform a simple arithmetic operation

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# Prompt user for input 
+current_age = int(input("How old are you?"))
+
+# Calculate the age of the user in 2050 assuming the current year is 2023.
+age = current_age + 27
+
+# Print out the result.
+print("In 2050, you will be", age, "years old.")
