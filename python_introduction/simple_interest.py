1eba37362d142f7078b0af8b26932f66735a3c14
Manfred Christoph Asare <asare1obeng@gmail.com>
2025-11-09 02:57:12 +0000

    Calculating simple interest in python

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Defining three variables in this file
+principal = 1000
+rate = 0.05
+time = 3 
+
+# Calculating the simple interest using the formular (I = P * R * T)
+interest = principal * rate * time
+
+# Print out the result
+print("The simple interest is:", interest)
