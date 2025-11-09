6ea10a0303306cbd4177ec8de97f5b12cb36743a
Manfred Christoph Asare <asare1obeng@gmail.com>
2025-11-09 02:46:02 +0000

    Performing basic arithmetic in Python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assigning values to variable
+number1 = 10
+number2 = 5
+
+# Performing arithmetic operations on numbers
+sum =  number1 + number2
+difference =  number1 - number2
+product = number1 * number2
+
+# Print results in human-readable format
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
