39ab1d7fb6907bddf8590af37252749aa5e0c1b5
Manfred Christoph Asare <asare1obeng@gmail.com>
2025-11-16 13:59:48 +0000

    Created a personal daily reminder that uses the concepts of control flow.

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,29 @@
+#Prompt for user input
+task = input("Enter your task: ")
+priority = input("Priority (high, medium, low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+#Process the task based on priority.
+match priority:
+  case "high":
+    reminder = f"Reminder: '{task}' is a high priority task"
+
+  case "medium":
+   reminder = f"Reminder: '{task}' is a medium priority task"
+
+  case "low":
+   reminder = f"Reminder: '{task}' is a low priority task"
+
+  case _:
+    reminder = f"Reminder: '{task}' is an Unknown priority task"
+
+#if statement to modify reminder if task is time bound
+if time_bound == 'yes':
+    reminder += f" that requires immediate attention today!"
+    
+else:
+   reminder += f". Consider completing it when you have free time."
+
+#Print output
+print(reminder)
+ 
